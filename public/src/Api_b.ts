2c465aec9dc45154bfa12581d95b4e2112bdae51
ForestChad <95200162+ForestChad@users.noreply.github.com>
2025-02-17 22:41:50 +0900

    redirection

--- public/src/Api_b.ts
+++ public/src/Api_b.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default class Api {
   static axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1/museumplay/gr",
+    baseURL: "http://localhost:4183/museumplay/gr",
     timeout: 1000,
   });
 
--- public/src/scenes/MyPage.ts
+++ public/src/scenes/MyPage.ts
@@ -221,13 +221,13 @@ export default class MyPage extends Phaser.Scene {
 
     this.baseUrl =
       process.env.NODE_ENV === "production"
-        ? "http://127.0.0.1:4183/museumplay/"
+        ? "http://localhost:4183/museumplay/"
         : process.env.BASE_URL
         ? process.env.BASE_URL
-        : "http://127.0.0.1:4183/museumplay/"; // 개발 환경에 따라 URL 변경
+        : "http://localhost:4183/museumplay/"; // 개발 환경에 따라 URL 변경
     this.linkBaseUrl =
       process.env.NODE_ENV === "production"
-        ? "http://127.0.0.1:4183/museumplay/goryeo-ro/"
+        ? "http://localhost:4183/museumplay/goryeo-ro/"
         : process.env.LINK_BASE_URL
         ? process.env.LINK_BASE_URL
         : "/"; // 개발 환경에 따라 URL 변경
